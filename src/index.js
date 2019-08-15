4718293e72a141aa2bc693b163fad2017584cb0c
hamzakhanane <khananehamza@gmail.com>
2019-08-15 10:22:26 -0400

    add lat and long

--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
-let arr = [];
-// d3.csv("./src/assets/output.csv", function (data) {
-//         new_obj = {}
-//         new_obj["ADDRESS"] = data["ADDRESS"];
-//         new_obj["SALE DATE"] = data["SALE DATE"];
-//         new_obj["SALE PRICE"] = data["SALE PRICE"];
-//         new_obj["ZIP CODE"] = data["ZIP CODE"];
-//         arr.push(new_obj);
-// })
+import { callbackify } from "util";
 
-d3.csv("./src/assets/output.csv").then(function (data) {
+window.arr = [];
+window.address_arr = [];
+window.address_lat_lng = [];
+
+
+d3.csv("./src/assets/output2.csv").then(function (data) {
 
     data.forEach((row) => {
 
-        new_obj = {}
+        let new_obj = {}
         new_obj["ADDRESS"] = row["ADDRESS"];
         new_obj["SALE DATE"] = row["SALE DATE"];
         new_obj["SALE PRICE"] = row["SALE PRICE"];
-        new_obj["ZIP CODE"] = row["ZIP CODE"];
-        arr.push(new_obj);
+        new_obj["lat"] = row["lat"];
+        new_obj["long"] = row["long"];
+
+        window.arr.push(new_obj);
     })
+    for(let i =0; i<window.arr.length; i++){
+        window.address_arr.push(Object.values(window.arr[i])[0])
+
+    }
+    window.uniq = [...new Set(window.address_arr)];
 
     
 
 
+
+
 });
+
+
+
+window.initMap = () => {
+    
+    var map = new google.maps.Map(d3.select("#map").node(), {
+        zoom: 12,
+        center: { lat: 40.730610, lng: -73.935242 },
+
+    });
+
+    window.map = map;
+
+}
 
 
 
